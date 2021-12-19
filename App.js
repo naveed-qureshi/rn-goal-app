df70e1fa89ab6bc313ef129d17b9413448484679
Muhammad Naveed <makhdoom47012@gmail.com>
2021-12-20 03:52:15 +0500

    Basic App complete.

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
   const [goals, AddGoals] = useState([])
   const [inputGoal, setInputGoal] = useState('')
+  const deleteItem = (goalId) => {
+    AddGoals((goals) => goals.filter((item) => item.key != goalId))
+  }
+
+const addGoalhandler = () => {
+  AddGoals((g) => [...g, {key: Math.random().toString(), data: inputGoal }])
+}
   return (
     <View style={{ padding: 50}}>
       <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
@@ -11,14 +18,18 @@ export default function App() {
           <TextInput style={{ borderWidth: 1, borderColor: 'black', padding: 5, marginBottom: 3 }} placeholder="Enter Goal" onChangeText={(enteredText) => setInputGoal(enteredText)} value={inputGoal} />
         </View>
         <View style={{ marginBottom: 3 }} >
-          <Button title="Add Goal" onPress={() => AddGoals([...goals, inputGoal])} />
+          <Button title="Add Goal" onPress={() => addGoalhandler()} />
         </View>
       </View>
-      <View>
-        <ScrollView>
-          { goals.map((goal, index) => <Text style={ { borderColor: 'black', borderWidth: 1, padding: 5, marginBottom: 3 } } key={index}>{goal} </Text>) }
-          </ScrollView>
-        </View>
+      <FlatList
+        keyExtractor={ item => item.key }
+        data={goals}
+        renderItem={(listItem) => (
+          <View >
+            <Text style={ { borderColor: 'black', borderWidth: 1, padding: 5, marginBottom: 3 } } onPress={() => deleteItem(listItem.item.key) } > { listItem.item.data } </Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
