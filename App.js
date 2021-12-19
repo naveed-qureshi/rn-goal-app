71728179357b0580e2a9cfa2cadf956d552012e7
Muhammad Naveed <makhdoom47012@gmail.com>
2021-12-20 01:22:12 +0500

    Added textinput and button

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <TextInput placeholder="Enter Goal"/>
+      <View style={{marginBottom: 3}} >
+        <Button title="Add Goal" />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
