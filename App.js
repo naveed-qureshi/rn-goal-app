80c66e881959458fb18cd931552c08f963c01cde
Muhammad Naveed <makhdoom47012@gmail.com>
2021-12-20 01:37:20 +0500

    Apply styles

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-      <TextInput style={{borderWidth: 1, borderColor: 'black', padding: 5, marginBottom: 3}} placeholder="Enter Goal"/>
+    <View style={{padding: 50, flexDirection: 'row', justifyContent: 'space-between' , alignItems: 'center'}}>
+      <View style={{flex: 1, marginRight: 5}}>
+        <TextInput style={{ borderWidth: 1, borderColor: 'black', padding: 5, marginBottom: 3}} placeholder="Enter Goal"/>
+      </View>
       <View style={{marginBottom: 3}} >
         <Button title="Add Goal" />
       </View>
