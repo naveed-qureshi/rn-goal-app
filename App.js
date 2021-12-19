1bfa10ae92c6e78c00bd5e0e2caf20911ab04142
Muhammad Naveed <makhdoom47012@gmail.com>
2021-12-20 01:47:57 +0500

    Added goals to array

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [goals, AddGoals] = useState([])
+  const [inputGoal, setInputGoal] = useState('')
   return (
-    <View style={{padding: 50, flexDirection: 'row', justifyContent: 'space-between' , alignItems: 'center'}}>
-      <View style={{flex: 1, marginRight: 5}}>
-        <TextInput style={{ borderWidth: 1, borderColor: 'black', padding: 5, marginBottom: 3}} placeholder="Enter Goal"/>
+    <View style={{ padding: 50, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+      <View style={{ flex: 1, marginRight: 5 }}>
+        <TextInput style={{ borderWidth: 1, borderColor: 'black', padding: 5, marginBottom: 3 }} placeholder="Enter Goal" onChangeText={(enteredText) => setInputGoal(enteredText)} value={inputGoal} />
       </View>
-      <View style={{marginBottom: 3}} >
-        <Button title="Add Goal" />
+      <View style={{ marginBottom: 3 }} >
+        <Button title="Add Goal" onPress={() => {
+          console.log(goals)
+          return AddGoals([...goals, inputGoal])
+        }} />
       </View>
     </View>
   );
