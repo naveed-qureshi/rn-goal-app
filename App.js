fdaa74eb66e523ea232b106ade4f3f385e79ee96
Muhammad Naveed <makhdoom47012@gmail.com>
2021-12-20 01:54:47 +0500

    Display goals

--- App.js
+++ App.js
@@ -5,15 +5,17 @@ export default function App() {
   const [goals, AddGoals] = useState([])
   const [inputGoal, setInputGoal] = useState('')
   return (
-    <View style={{ padding: 50, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-      <View style={{ flex: 1, marginRight: 5 }}>
-        <TextInput style={{ borderWidth: 1, borderColor: 'black', padding: 5, marginBottom: 3 }} placeholder="Enter Goal" onChangeText={(enteredText) => setInputGoal(enteredText)} value={inputGoal} />
+    <View style={{ padding: 50}}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <View style={{ flex: 1, marginRight: 5 }}>
+          <TextInput style={{ borderWidth: 1, borderColor: 'black', padding: 5, marginBottom: 3 }} placeholder="Enter Goal" onChangeText={(enteredText) => setInputGoal(enteredText)} value={inputGoal} />
+        </View>
+        <View style={{ marginBottom: 3 }} >
+          <Button title="Add Goal" onPress={() => AddGoals([...goals, inputGoal])} />
+        </View>
       </View>
-      <View style={{ marginBottom: 3 }} >
-        <Button title="Add Goal" onPress={() => {
-          console.log(goals)
-          return AddGoals([...goals, inputGoal])
-        }} />
+      <View>
+        {goals.map((goal, index) => <Text key={index}>{goal}</Text>)}
       </View>
     </View>
   );
